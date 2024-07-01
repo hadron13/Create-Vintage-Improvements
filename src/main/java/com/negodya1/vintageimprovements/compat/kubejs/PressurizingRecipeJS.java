d0f9339bf1ea3be40b5d838e7ef4e237d070a78d
hadron13 <carlosbpfonseca@gmail.com>
2024-07-01 16:20:02 -0300

    Added untracked files and changed secondaryIn/Output to secondaryFluidIn/Output in Pressurizing recipe

--- src/main/java/com/negodya1/vintageimprovements/compat/kubejs/PressurizingRecipeJS.java
+++ src/main/java/com/negodya1/vintageimprovements/compat/kubejs/PressurizingRecipeJS.java
@@ -2,12 +2,12 @@ package com.negodya1.vintageimprovements.compat.kubejs;
 
 public class PressurizingRecipeJS extends ProcessingRecipeJS{
 
-    public PressurizingRecipeJS secondaryInput(int slot){
+    public PressurizingRecipeJS secondaryFluidInput(int slot){
         json.addProperty("secondaryFluidInputs", slot);
         save();
         return this;
     }
-    public PressurizingRecipeJS secondaryOutput(int slot){
+    public PressurizingRecipeJS secondaryFluidOutput(int slot){
         json.addProperty("secondaryFluidResults", slot);
         save();
         return this;
